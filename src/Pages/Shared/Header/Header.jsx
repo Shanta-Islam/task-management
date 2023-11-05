3fc4411b3d4b8276f35b9e0c1e4fdf491b76dab9
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-11-05 22:53:32 +0600

    firebase authentication complete

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,24 +1,43 @@
 
-import { useEffect, useState } from "react";
-import {  Link, NavLink } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthProvider";
 
 
 
 const Header = () => {
-    // const { user, logOut } = useContext(AuthContext);
-    // const handleSignOut = () => {
-    //     logOut()
-    //         .then(() => {
-    //             toast.success('Successfully Log Out');
-    //         })
-    //         .catch()
-    // }
+    const { user, logOut } = useContext(AuthContext);
+    const handleSignOut = () => {
+        logOut()
+            .then(() => {
+                toast.success('Successfully Log Out');
+            })
+            .catch()
+    }
     const links = <>
         <li><NavLink to="/" className={({ isActive, isPending }) =>
             isPending ? "pending" : isActive ? "hover:bg-cyan-600 bg-cyan-600 hover:text-white text-white px-5 py-2 text-md rounded" : "hover:bg-cyan-600 hover:text-white text-white px-5 py-2 mx-2 text-md rounded"}>Home</NavLink></li>
-        <li><NavLink to="/all-assignments" className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "hover:bg-cyan-600 bg-cyan-600 hover:text-white text-white px-5 py-2 text-md rounded" : "hover:bg-cyan-600 hover:text-white text-white px-5  py-2 mx-2 text-md rounded"}>Assignments</NavLink></li>
-       
+
+        {
+            user ?
+
+                <div className="flex">
+                    <li><NavLink to="/all-assignments" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "hover:bg-cyan-600 bg-cyan-600 hover:text-white text-white px-5 py-2 text-md rounded" : "hover:bg-cyan-600 hover:text-white text-white px-5  py-2 mx-2 text-md rounded"}>Assignments</NavLink></li>
+                    <li className="mx-2"><NavLink to="/create-assignments" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-white hover:text-white focus:text-white bg-transparent underline" : "text-white hover:text-white"}>Create Assignments</NavLink></li>
+                    <li className="mx-3"><NavLink to="/my-assignments" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-white hover:text-white focus:text-white bg-transparent underline" : "text-white hover:text-white"}>My Assignments</NavLink></li>
+                    <li className="mx-3"><NavLink to="/submitted-assignments" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-white hover:text-white focus:text-white bg-transparent underline" : "text-white hover:text-white"}>Submitted Assignments</NavLink></li>
+
+                </div>
+                :
+                <li><NavLink to="/all-assignments" className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "hover:bg-cyan-600 bg-cyan-600 hover:text-white text-white px-5 py-2 text-md rounded" : "hover:bg-cyan-600 hover:text-white text-white px-5  py-2 mx-2 text-md rounded"}>Assignments</NavLink></li>
+               
+        }
     </>
 
 
@@ -39,7 +58,7 @@ const Header = () => {
     // }, [theme]);
 
     const [headerColor, setHeaderColor] = useState("")
-    
+
     const listenScrollEvent = () => {
         window.scrollY > 10
             ? setHeaderColor("#164863")
@@ -49,6 +68,16 @@ const Header = () => {
     useEffect(() => {
         window.addEventListener("scroll", listenScrollEvent)
     })
+
+    const [show, setShow] = useState(false);
+
+    const showOverlay = () => {
+        setShow(true);
+    };
+
+    const hideOverlay = () => {
+        setShow(false);
+    };
 
     return (
         <div className="navbar fixed top-0 z-50" style={{ background: headerColor }}>
@@ -74,7 +103,47 @@ const Header = () => {
                     {links}
                 </ul>
             </div>
-            
+            <div className="navbar-end lg:flex items-center">
+                {/* <button className="btn btn-square btn-ghost">
+                    <label className="swap swap-rotate w-6 h-6">
+                        <input type="checkbox"
+                            onChange={handleToggle}
+                            checked={theme === "light" ? false : true} />
+                        <HiSun className="w-6 h-6 swap-on"></HiSun>
+                        <HiMoon className="w-6 h-6 swap-off"></HiMoon>
+                    </label>
+                </button> */}
+                {
+                    user ?
+                        <ul className="menu-horizontal px-3">
+                            <li className="mx-3" onMouseOver={showOverlay}
+                                onMouseLeave={hideOverlay}><div className="avatar flex-col" >
+                                    <div className="w-8 rounded-full" >
+                                        <img src={user?.photoURL ? user?.photoURL : 'https://i.ibb.co/X2xMzwL/defultuser.png'} />
+
+                                    </div>
+                                    <ul>
+                                        <li className="text-white">{show && (
+                                            <div>
+                                                <p>{user?.displayName ? user?.displayName : "User"}</p>
+                                            </div>
+                                        )}</li>
+                                    </ul>
+
+                                </div></li>
+                            <li className="mx-3 cursor-pointer text-white" onClick={handleSignOut}><a>SignOut</a></li>
+                        </ul>
+                        :
+                        <ul className="menu menu-horizontal px-1">
+                            <li><NavLink to="/login" className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-white hover:text-white focus:text-white bg-transparent underline" : "text-white hover:text-white"}>Login</NavLink></li>
+                            <li><NavLink to="/register" className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-white hover:text-white focus:text-white bg-transparent underline" : "text-white hover:text-white"}>Register</NavLink></li>
+                        </ul>
+
+                }
+            </div>
+
         </div>
     );
 };
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,7 +1,10 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
-
+import Login from "../Pages/Login/Login";
+import Assignments from "../Pages/Assignments/Assignments";
+import Register from "../Pages/Register/Register";
+import CreateAssignments from "../Pages/CreateAssignments/CreateAssignments";
 const router = createBrowserRouter([
     {
       path: "/",
@@ -11,11 +14,26 @@ const router = createBrowserRouter([
           path: "/",
           element: <Home></Home>,
         },
-       
+        {
+          path: "/all-assignments",
+          element: <Assignments></Assignments>,
+        },
+        {
+          path: "/create-assignments",
+          element: <CreateAssignments></CreateAssignments>
+        }
         
-        
-      ]
+      ],
+      
     },
+    {
+      path: "/login",
+      element: <Login></Login>
+    },
+    {
+      path: "/register",
+      element: <Register></Register>
+    }
   ]);
 
   export default router;
