7d67e5071b3043cd8be7d8bc6ce36279c08edf31
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-11-08 22:53:30 +0600

    using framer motion

---
+++ src/Animations/Animations.jsx
@@ -0,0 +1,13 @@
+
+export const fadeInLeft ={
+    hidden: {x: -100, opacity: 0},
+    show: {x: 0, opacity: 1} 
+}
+export const fadeInRight ={
+    hidden: {x: 100, opacity: 0},
+    show: {x: 0, opacity: 1}
+}
+export const fadeInTop ={
+    hidden: {y: 20, opacity: 0},
+    show: {y: 0, opacity: 1},
+}
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,14 +1,24 @@
-
+import { motion } from "framer-motion";
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-right-top bg-no-repeat" style={{ backgroundImage: 'url(https://i.ibb.co/qWrLHNn/pexels-andrea-piacquadio-3884186.jpg)'}}>
+        <div className="hero min-h-screen bg-right-top bg-no-repeat" style={{ backgroundImage: 'url(https://i.ibb.co/qWrLHNn/pexels-andrea-piacquadio-3884186.jpg)' }}>
             <div className="hero-overlay bg-opacity-60"></div>
+
             <div className="hero-content text-center text-neutral-content">
-                <div>
-                    <h1 className="mb-5 text-5xl font-bold">Study with students from all over the world </h1>
-                    <p className="mb-5">Join the largest global student community online and say goodbye to lack of motivation</p>
-                </div>
+                <motion.div 
+                               
+                               animate={{ rotate: 360 }}
+  transition={{ type: 'spring', restDelta: 0.5 }}
+                               >
+                    <div>
+                        <h1 className="mb-5 text-5xl font-bold">Study with students from all over the world </h1>
+                        <p className="mb-5">Join the largest global student community online and say goodbye to lack of motivation</p>
+                    </div>
+                </motion.div>
+
             </div>
+
+
         </div>
     );
 };
